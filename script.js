5d1da71b8979265ee2a11fcdf2fc8c18339eaf5f
shahzebinfo <shahzebahmad2024@gmail.com>
2025-08-09 03:52:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-const imageFolder = 'image/product1/';
-const images = [
-  'A.png',
-  'B.png',
-  'C.png',
-  'D.png'
-  // Apne images ke naam yahan add karen
-];
+const imageFolder = 'images/product1/';
+const images = ['A.png', 'B.png', 'C.png', 'D.png'];
 
 const container = document.getElementById('imageScroll');
 images.forEach(imgName => {
@@ -14,4 +8,3 @@ images.forEach(imgName => {
   img.alt = 'Product Image';
   container.appendChild(img);
 });
-
